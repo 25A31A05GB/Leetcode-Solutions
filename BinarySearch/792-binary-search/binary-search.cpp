d659aa2ac900c47891fe3f299d1d93b397206bbd
Jayaram Sappa <244126196+25A31A05GB@users.noreply.github.com>
2026-08-29 17:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (81.31%) - LeetSync

--- BinarySearch/792-binary-search/binary-search.cpp
+++ BinarySearch/792-binary-search/binary-search.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-// normal binary search
+    // normal binary search
     int search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size() - 1;
+
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2; // Find middle
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
-            }
+            if (nums[mid] == target)
+                return mid; // Found
 
-            else {
-                low = mid + 1;
-            }
+            else if (nums[mid] > target)
+                high = mid - 1; // Left half
+
+            else
+                low = mid + 1; // Right half
         }
-        return -1;
+
+        return -1; // Not found
     }
 };
